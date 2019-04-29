be7f22f27a13df20869f4d48e66cc3dfe28bb69a
Nicholas Machin <nicholasjohnmachin@gmail.com>
2019-04-29 20:25:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_array)
+  name_array.each do |name|
+    badge_maker(name)
+  end
+end
+
+def assign_rooms(list_speakers)
+  
